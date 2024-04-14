bbd4192f478b7bf37dfe236b604d3d7d17b47c0a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-14 14:42:40 +0600

    Banner section added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,13 @@
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full rounded">
       <div id="slide1" className="carousel-item relative w-full">
         <img
-          src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-          className="w-full"
+          src="https://i.ibb.co/0YTyRpw/first-Banner-Image.jpg"
+          className="w-full h-2/3 bg-center"
         />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
+          <a href="#slide3" className="btn btn-circle">
             ❮
           </a>
           <a href="#slide2" className="btn btn-circle">
@@ -17,8 +17,8 @@ const Banner = () => {
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img
-          src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-          className="w-full"
+          src="https://i.ibb.co/ynLCB1q/second-Banner-Image.jpg"
+          className="w-full h-2/3 bg-center"
         />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
@@ -31,25 +31,11 @@ const Banner = () => {
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img
-          src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
-          className="w-full"
+          src="https://i.ibb.co/FqzntSb/third-Banner-Image.png"
+          className="w-full h-2/3 bg-center"
         />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
             ❮
           </a>
           <a href="#slide1" className="btn btn-circle">
