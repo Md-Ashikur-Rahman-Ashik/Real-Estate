aa1796fffca8be51d8ae7d32d52cfd2f697a99b0
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-15 18:01:38 +0600

    Swiper slider added with functionality

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,48 +1,33 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation } from "swiper/modules";
+
 const Banner = () => {
   return (
-    <div className="carousel rounded-xl mx-10">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/0YTyRpw/first-Banner-Image.jpg"
-          className="w-full h-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/ynLCB1q/second-Banner-Image.jpg"
-          className="w-full h-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/FqzntSb/third-Banner-Image.png"
-          className="w-full h-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+    <div className="mx-10">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/0YTyRpw/first-Banner-Image.jpg"
+            className="rounded-xl"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://i.ibb.co/ynLCB1q/second-Banner-Image.jpg"
+            className="rounded-xl"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co/FqzntSb/third-Banner-Image.png" alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
